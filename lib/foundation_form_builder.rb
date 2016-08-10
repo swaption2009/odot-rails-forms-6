734474afb697f3699611cda4ffaa8a0abaf06ee3
swaption2009 <swaption2009@gmail.com>
2016-08-09 20:07:20 -0700

    create text_field form builder

--- lib/foundation_form_builder.rb
+++ lib/foundation_form_builder.rb
@@ -5,5 +5,26 @@ class FoundationFormBuilder < ActionView::Helpers::FormBuilder
 
   attr_accessor :output_buffer
 
+  def text_field(attribute, options={})
+    options[:label] ||= attribute
+    label_text ||= options.delete(:label).to_s.titlecase
+    wrapper do
+      label(attribute, label_text) +
+      super(attribute, options)
+    end
+  end
+
+  def submit(text, options={})
+    options[:class] ||= "button radius expand"
+    wrapper do
+      super(text, options)
+    end
+  end
+
+  def wrapper(options={}, &block)
+    content_tag(:div, class: "row") do
+      content_tag(:div, capture(&block), class: "small-12 columns")
+    end
+  end
 
 end
